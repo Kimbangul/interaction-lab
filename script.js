afd8de14ba1cf73f0924bb24a713b8a7585d9bef
Kimbangul <highcolor_12@naver.com>
2021-05-12 12:58:07 +0900

    step 2 슬라이드 부분 작업중

--- script.js
+++ script.js
@@ -13,7 +13,29 @@ $(document).ready(function() {
 
     }
 
+    function slickSlide_init(){
+        $('#step-2 .card-slide').slick({
+            slide: 'div.card',
+            infinite: true,
+            slidesToShow: 1,
+            centerPadding: '33%',
+            slidesToScroll: 1,
+            speed: 1000,
+            prevArrow: "<button type='button' class='slick-prev'><img src='img/btn-slide-left.png' alt='이전 슬라이드로'></button>", // 이전 화살표 모양 설정
+            nextArrow: "<button type='button' class='slick-next'><img src='img/btn-slide-right.png' alt='다음 슬라이드로'></button>",
+            dots: false,
+            autoplay: false,
+            centerMode: true,
+          
+            autoplaySpeed: 7000,
+            pauseOnHover: false,
+            vertical: false,          
+            draggable: true
+        });
+    }
+
     fullpage_init();
+    slickSlide_init();
 
 
 });
