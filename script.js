8b8b25891b75d84e03994ae00f712bd6e310ac42
Kimbangul <highcolor_12@naver.com>
2021-05-12 23:32:28 +0900

    step 3 섹션 레이아웃 작업

--- script.js
+++ script.js
@@ -16,22 +16,43 @@ $(document).ready(function() {
     function slickSlide_init(){
         $('#step-2 .card-slide').slick({
             slide: 'div.card',
-            infinite: true,
-            slidesToShow: 1,
-            centerPadding: '33%',
+            infinite: true,                   
             slidesToScroll: 1,
             speed: 1000,
             prevArrow: "<button type='button' class='slick-prev'><img src='img/btn-slide-left.png' alt='이전 슬라이드로'></button>", // 이전 화살표 모양 설정
             nextArrow: "<button type='button' class='slick-next'><img src='img/btn-slide-right.png' alt='다음 슬라이드로'></button>",
             dots: false,
             autoplay: false,
-            centerMode: true,
-          
+            centerMode: true,   
+            centerPadding: '33.3%',         
             autoplaySpeed: 7000,
             pauseOnHover: false,
             vertical: false,          
             draggable: true
         });
+
+        $('#step-2 .card-slide').on("afterChange",function(){
+            var cardNameTargetElem = document.querySelector('#step-2 .card-name');
+            var commentTargetElem = document.querySelector('#step-2 .con > div.comment > div.text-box');
+
+            var currentSlideImg = document.querySelector('#step-2 .card-slide .slick-current > img');
+            var cardName = currentSlideImg.dataset['name'];
+            var comment = currentSlideImg.dataset['comment'];
+
+            cardNameTargetElem.innerHTML = cardName;
+            commentTargetElem.innerHTML = comment;
+        });
+    }
+
+    function insertText(){
+       
+
+        function cardNameChange(){
+            var currentSlideImg = document.querySelector('#step-2 .card-slide .slick-current > img')
+            var cardName = currentSlideImg.dataset['name'];
+            targetElem.innerHTML = cardName;
+        }
+
     }
 
     fullpage_init();
