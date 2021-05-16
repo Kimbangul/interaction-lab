54b5019808c8a2422f971a369ddec164a6a1c5e2
Kimbangul <highcolor_12@naver.com>
2021-05-17 00:55:19 +0900

    step 4 레이아웃 작업중

--- js/script.js
+++ js/script.js
@@ -14,61 +14,91 @@ $(document).ready(function() {
                 step3Animate_init(index);
             }
 
+
         });
 
     }
 
     function slickSlide_init(){
-        $('#step-2 .card-slide').slick({
-            slide: 'div.card',
-            infinite: true,                   
-            slidesToScroll: 1,
-            speed: 1000,
-            prevArrow: "<button type='button' class='slick-prev'><img src='img/btn-slide-left.png' alt='이전 슬라이드로'></button>", // 이전 화살표 모양 설정
-            nextArrow: "<button type='button' class='slick-next'><img src='img/btn-slide-right.png' alt='다음 슬라이드로'></button>",
-            dots: false,
-            autoplay: false,
-            centerMode: true,   
-            centerPadding: '33.3%',         
-            autoplaySpeed: 7000,
-            pauseOnHover: false,
-            vertical: false,          
-            draggable: true
-        });
+        function step2Slide(){
+            $('#step-2 .card-slide').slick({
+                slide: 'div.card',
+                infinite: true,                   
+                slidesToScroll: 1,
+                speed: 1000,
+                prevArrow: "<button type='button' class='slick-prev'><img src='img/btn-slide-left.png' alt='이전 슬라이드로'></button>", // 이전 화살표 모양 설정
+                nextArrow: "<button type='button' class='slick-next'><img src='img/btn-slide-right.png' alt='다음 슬라이드로'></button>",
+                dots: false,
+                autoplay: false,
+                centerMode: true,   
+                centerPadding: '33.3%',         
+                autoplaySpeed: 7000,
+                pauseOnHover: false,
+                vertical: false,          
+                draggable: true
+            });
+    
+            $('#step-2 .card-slide').on("afterChange",function(){
+                var cardNameTargetElem = document.querySelector('#step-2 .card-name');
+                var commentTargetElem = document.querySelector('#step-2 .con > div.comment > div.text-box');
+    
+                var currentSlideImg = document.querySelector('#step-2 .card-slide .slick-current > img');
+                var cardName = currentSlideImg.dataset['name'];
+                var comment = currentSlideImg.dataset['comment'];
+    
+                cardNameTargetElem.innerHTML = cardName;
+                commentTargetElem.innerHTML = comment;
+            });
+        }
 
-        $('#step-2 .card-slide').on("afterChange",function(){
-            var cardNameTargetElem = document.querySelector('#step-2 .card-name');
-            var commentTargetElem = document.querySelector('#step-2 .con > div.comment > div.text-box');
+        function step4Slide(){
+            $('#step-4 .slider').slick({
+                slide: 'div.section-slide',
+                infinite: true,                   
+                slidesToScroll: 1,
+                speed: 1000,
+                prevArrow: "<button type='button' class='slick-prev'><img src='img/btn-slide-left.png' alt='이전 슬라이드로'></button>", // 이전 화살표 모양 설정
+                nextArrow: "<button type='button' class='slick-next'><img src='img/btn-slide-right.png' alt='다음 슬라이드로'></button>",
+                dots: false,
+                autoplay: false,    
+                autoplaySpeed: 7000,
+                pauseOnHover: false,
+                vertical: false,          
+                draggable: true
+            });
+        }
 
-            var currentSlideImg = document.querySelector('#step-2 .card-slide .slick-current > img');
-            var cardName = currentSlideImg.dataset['name'];
-            var comment = currentSlideImg.dataset['comment'];
-
-            cardNameTargetElem.innerHTML = cardName;
-            commentTargetElem.innerHTML = comment;
-        });
+        step2Slide();
+        step4Slide();
     }
 
     function step3Animate_init(index){  
-        console.log(index);  
-        if ($('#step-3').hasClass('active')){
-            console.log('true');
+       
+        var balloonPositionFirst = $("#cat-balloon").css("top");        
+        if (index.index == 3){
+            
             $(window).bind('mousewheel', function(e){
-                if(e.originalEvent.wheelDelta /120 > 0) {
-                  console.log(e.originalEvent.wheelDelta);
- 
-                }
-                else{
-                  console.log(e.originalEvent.wheelDelta);
+                
+                var currentPosition = parseInt($("#cat-balloon").css("top"));
+                var scrollDistance = 250;
 
+                if(e.originalEvent.wheelDelta /120 > 0) {
+                    if (parseInt(currentPosition - scrollDistance) >= parseInt(balloonPositionFirst)){
+                        $("#cat-balloon").stop().animate({"top":currentPosition - scrollDistance + "px"},1000);
+                        }
+                    }
+                else{
+                    // scroll down                   
+                  if (parseInt(currentPosition + scrollDistance) <= parseInt(balloonPositionFirst) + parseInt(scrollDistance * 3) ){
+                    $("#cat-balloon").stop().animate({"top":currentPosition + scrollDistance + "px"},1000);
+                  }
+                  
                 }
-              });
-            var currentPosition = parseInt($("#cat-balloon").css("top")); 
-            $(window).scroll(function() { 
-                var position = $(window).scrollTop(); 
-                console.log(position);
-                $("#cat-balloon").stop().animate({"top":position+currentPosition+"px"},1000); 
-            });
+              });        
+        }
+        else{
+            console.log(index.index);
+            $("#cat-balloon").css("top","20%");
         }
 
 
